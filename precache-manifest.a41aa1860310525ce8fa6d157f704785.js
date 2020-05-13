c8fea72f0d792733def0971790e66366517fbf7f
David Yudha <davidyudha@mail.ugm.ac.id>
2020-05-13 10:20:11 +0700

    Updates

---
+++ precache-manifest.a41aa1860310525ce8fa6d157f704785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a8ed6bda69f35a9b1d840be5b665e3",
+    "url": "/to-do-notes/index.html"
+  },
+  {
+    "revision": "5d44c390b8a4bc24afc6",
+    "url": "/to-do-notes/static/css/main.f970b54f.chunk.css"
+  },
+  {
+    "revision": "089584d0323d1a8e85c2",
+    "url": "/to-do-notes/static/js/2.a5c9c664.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/to-do-notes/static/js/2.a5c9c664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d44c390b8a4bc24afc6",
+    "url": "/to-do-notes/static/js/main.59941185.chunk.js"
+  },
+  {
+    "revision": "a20dd6bd5f2d5c998cdc",
+    "url": "/to-do-notes/static/js/runtime-main.fe666f04.js"
+  }
+]);
